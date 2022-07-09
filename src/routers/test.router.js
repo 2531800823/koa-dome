5e43cb371b22ba2242049ff3c2d0682b1004f98a
刘士朋 <2531800823@qq.com>
2022-07-09 17:41:10 +0800

    使用 openssl 生成公钥私钥来加密

--- src/routers/test.router.js
+++ src/routers/test.router.js
@@ -25,13 +25,49 @@ testRouter.get("/jwt", (ctx, next) => {
 
 // 测试 token
 testRouter.get("/test/jwt", (ctx, next) => {
-  //   console.log(ctx.headers.authorization);
   const authorization = ctx.headers.authorization;
   const token = authorization.replace("Bearer ", "");
   console.log(token);
 
   try {
     const result = jwt.verify(token, LIU_KEY);
+    ctx.body = result;
+  } catch (error) {
+    ctx.body = "token 无效";
+  }
+});
+
+const fs = require("fs");
+const path = require("path");
+const PRIVATE_KEY = fs.readFileSync(
+  path.resolve(__dirname, "../../keys/private.key")
+);
+const PUBLIC_KEY = fs.readFileSync(
+  path.resolve(__dirname, "../../keys/public.key")
+);
+
+// 颁发 token 公钥私钥
+testRouter.get("/jwt2", (ctx, next) => {
+  const user = { id: 10, name: "liu" };
+  // 可以 传递一个 buffer 的密钥
+  const token = jwt.sign(user, PRIVATE_KEY, {
+    expiresIn: 30, // 过期时间，单位 秒
+    algorithm: "RS256", // 指定非对称加密算法
+  });
+
+  ctx.body = token;
+});
+
+// 验证 token 公钥私钥
+testRouter.get("/test/jwt2", (ctx, next) => {
+  const authorization = ctx.headers.authorization;
+  const token = authorization.replace("Bearer ", "");
+  console.log(token);
+
+  try {
+    const result = jwt.verify(token, PUBLIC_KEY, {
+      algorithm: ["RS256"], // 指定非对称加密算法,可以传递多个算法
+    });
     ctx.body = result;
   } catch (error) {
     ctx.body = "token 无效";

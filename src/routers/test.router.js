6a531d18c3602ea6f006e837eebce9fb486e38bf
刘士朋 <2531800823@qq.com>
2022-07-09 16:59:26 +0800

    token 测试

--- src/routers/test.router.js
+++ src/routers/test.router.js
@@ -1,4 +1,5 @@
 const Router = require("koa-router");
+const jwt = require("jsonwebtoken");
 
 const testRouter = new Router();
 
@@ -8,7 +9,33 @@ testRouter.get("/cookie", (ctx, next) => {
   ctx.session.users = "liushipeng";
   ctx.session.a = "liushipeng1";
 
-  ctx.body = `<h1>{成功}</h1>`;
+  ctx.body = `<h1>测试 cookie 成功</h1>`;
+});
+
+// 发送token
+const LIU_KEY = "liushipeng"; // 密钥
+testRouter.get("/jwt", (ctx, next) => {
+  const user = { id: 10, name: "liu" };
+  const token = jwt.sign(user, LIU_KEY, {
+    expiresIn: 30, // 过期时间，单位 秒
+  });
+
+  ctx.body = token;
+});
+
+// 测试 token
+testRouter.get("/test/jwt", (ctx, next) => {
+  //   console.log(ctx.headers.authorization);
+  const authorization = ctx.headers.authorization;
+  const token = authorization.replace("Bearer ", "");
+  console.log(token);
+
+  try {
+    const result = jwt.verify(token, LIU_KEY);
+    ctx.body = result;
+  } catch (error) {
+    ctx.body = "token 无效";
+  }
 });
 
 module.exports = testRouter;
